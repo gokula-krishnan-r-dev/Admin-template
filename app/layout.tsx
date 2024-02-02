d082fdcbbe9c9fb6779c13b54cd5dd053c70cf5d
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-02-02 22:06:43 +0530

    api

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,12 @@ import { Toaster } from "@/components/ui/toaster";
 import "@uploadthing/react/styles.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { getServerSession } from "next-auth";
 const inter = Inter({ subsets: ["latin"] });
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
-
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} overflow-hidden`}>
