77b04db1c9ffdd13f1050f3eec2b21063382695b
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-02-02 22:03:16 +0530

    demo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} overflow-hidden`}>
-        <Providers session={session}>
+        <Providers>
           <Toaster />
 
           {children}
